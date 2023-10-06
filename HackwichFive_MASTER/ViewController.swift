ce14fafc1a637f5879c460967144b54f1d34447f
marionatuhwo <ano@hawaii.edu>
2023-10-06 08:33:15 -1000

    Made changes to the VC code.

--- HackwichFive_MASTER/ViewController.swift
+++ HackwichFive_MASTER/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //Part 3
+    //HW 5 P1 //Part 3
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var bottomLabel: UILabel!
     //Part 5
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     var favoriteFoodsArray = ["Sushi", "Karaage Chicken", "Ice Cream", "Laulau"]
     
     //================================
-    //HW5 Part 2 - Part 1
+    //HW5 P1 Part 2 - Part 1
     
     @IBOutlet weak var buttonLabel: UIButton!
     
@@ -29,32 +29,28 @@ class ViewController: UIViewController {
     }
     
 //HW 5 Part 1 - Part 6
-    /*
-    @IBAction func myFavoriteFoodButtonPressed(_ sender: Any) {
+
+   /* @IBAction func myFavoriteFoodButtonPressed(_ sender: Any) {
         bottomLabel.text = favoriteFoodsArray[0]
-    }*/
+    } */
     
-    
-    @IBAction func myFavoriteFoodButtonPressed(_ sender: Any) {
+    //HW 5 Part 2
+   @IBAction func myFavoriteFoodButtonPressed(_ sender: Any)
+    {
         
-        //Comment #1: if currentIndex is less than the "favoriteFoodsArray" array count
                    if self.currentIndex < self.favoriteFoodsArray.count
                    {
-                       // Comment #2: set bottomLabeltext to item in the array at currentIndex
                        bottomLabel.text = favoriteFoodsArray[currentIndex]
-                       //Comment #3:set the title of the button to "Next"
                        buttonLabel.setTitle("Next", for: UIControl.State.normal)
-                       //Comment #4:increment currentIndex by 1
+                       
                        currentIndex+=1
                    }
                    else
                    {
-                       //Comment #5:print the string: "Button has been disabled"
                        print("Button has been disabled")
-                       //Comment #6:disable the button
                        (buttonLabel!).isEnabled=false
                    }
-    }
+   }
     
 }
 
